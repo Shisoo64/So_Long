44ff55ed2d75a16214dfc356abbf33d94ff05c4f
Romil <47531544+Shisoo64@users.noreply.github.com>
2022-05-16 02:47:28 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,20 +1,34 @@
 #include "so_long.h"
 
+typedef struct	s_vars {
+	void	*mlx;
+	void	*win;
+}				t_vars;
+
+void	printRow(t_vars vars, void *img, char *str)
+{
+	int	i;
+
+	i = -1;
+	while(str[++i])
+		if(str[i] == '1')
+			mlx_put_image_to_window(vars.mlx, vars.win, img, 16 * i, 0);
+}
+
 int	main(void)
 {
-	void	*mlx;
-	void	*mlx_win;
-	void	*img;
+	t_vars	vars;
+	void	*img_floor;
 	char	*relative_path = "./sprites/floor.xpm";
 	int		img_width;
 	int		img_height;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	img = mlx_xpm_file_to_image(mlx, relative_path, &img_width, &img_height);
-	mlx_put_image_to_window(mlx, mlx_win, img, 0, 0);
-	mlx_put_image_to_window(mlx, mlx_win, img, 0, 16);
-	mlx_put_image_to_window(mlx, mlx_win, img, 16, 0);
-	mlx_put_image_to_window(mlx, mlx_win, img, 16, 16);
-	mlx_loop(mlx);
+	vars.mlx = mlx_init();
+	img_floor = mlx_xpm_file_to_image(vars.mlx, relative_path, &img_width, &img_height);
+	
+	vars.win = mlx_new_window(vars.mlx, img_width*16, img_height*16, "Super Romil!");
+	
+	printRow(vars, img_floor, "000111001010");
+	
+	mlx_loop(vars.mlx);
 }
